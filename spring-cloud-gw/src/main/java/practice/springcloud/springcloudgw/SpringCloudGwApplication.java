a5564a9e21d97ed41112e714e87cbc176bac1bc8
xi_jjun <rlawowns97@naver.com>
2022-09-10 18:56:06 +0900

    spring cloud gate way init

---
+++ spring-cloud-gw/src/main/java/practice/springcloud/springcloudgw/SpringCloudGwApplication.java
@@ -0,0 +1,13 @@
+package practice.springcloud.springcloudgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGwApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-gw/src/test/java/practice/springcloud/springcloudgw/SpringCloudGwApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.springcloud.springcloudgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
